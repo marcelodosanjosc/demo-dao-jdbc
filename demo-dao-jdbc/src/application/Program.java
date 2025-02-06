0f348f46a160a054154668b0688cce2c20b3eb3d
marcelo.caldas <marcelo.caldas@fpf.com>
2025-02-06 17:52:23 -0400

    Reusing instantiation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,13 +1,8 @@
 package application;
 
-import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.sql.Connection;
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
